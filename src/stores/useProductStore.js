e331dbe11751b9074acef11ef5760f33c2e6f3da
Anna <anna.cano.7e7@itb.cat>
2025-01-28 19:08:47 +0100

    add: action to product store

--- src/stores/useProductStore.js
+++ src/stores/useProductStore.js
@@ -1,14 +1,17 @@
 import { defineStore } from "pinia";
-import products from '@/data/products.json'
-
 // nom i option object
 export const useProductStore=defineStore('ProductStore',{
     //state
     state:()=>{
         return{
-            products,
+            products:[]
+        }
+    },
+    //actions
+    actions:{
+        async fill(){
+            this.products=(await import ('@/data/products.json')).default ;
         }
     }
-    //actions
     //getters
 })
